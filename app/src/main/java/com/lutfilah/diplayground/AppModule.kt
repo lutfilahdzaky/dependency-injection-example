c928b2801e59758f5a840d2205bc6512e07503ec
Lutfilah Dzaky <lutfilahdzaky12@gmail.com>
2022-04-25 23:31:40 +0700

    Add AppModule for Hilt dependency

---
+++ app/src/main/java/com/lutfilah/diplayground/AppModule.kt
@@ -0,0 +1,12 @@
+package com.lutfilah.diplayground
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/* Anotasi @Module
+* Berguna untuk memberitahukan Hilt class yang diberikan anotasi merupakan modul dependency untuk Hilt
+*/
+@Module
+object AppModule {
+}
--- app/src/main/java/com/lutfilah/diplayground/BaseApplication.kt
+++ app/src/main/java/com/lutfilah/diplayground/BaseApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 /* Selalu membuat Application class
-* karena dibalik layar hilt perlu mengenerate class yang kita butuhkan.
+* karena dibalik layar Hilt perlu mengenerate class yang kita butuhkan.
 * Jangan lupa untuk menambahkan anotasi @HiltAndroidApp
 * serta mendeklarasikan Application class di manifest
 */
