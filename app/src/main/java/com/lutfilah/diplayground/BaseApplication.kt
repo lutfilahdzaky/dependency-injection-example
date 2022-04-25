2403ffdc568f962704af10fc74306f100e7f0def
Lutfilah Dzaky <lutfilahdzaky12@gmail.com>
2022-04-25 22:55:56 +0700

    Add Application class for Hilt

---
+++ app/src/main/java/com/lutfilah/diplayground/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.lutfilah.diplayground
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/* Selalu membuat Application class
+* karena dibalik layar hilt perlu mengenerate class yang kita butuhkan.
+* Jangan lupa untuk menambahkan anotasi @HiltAndroidApp
+* serta mendeklarasikan Application class di manifest
+*/
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
