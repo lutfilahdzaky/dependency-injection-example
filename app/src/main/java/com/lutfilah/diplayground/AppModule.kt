c70944f0406ea51d0eca8d102f86d9636344e358
Lutfilah Dzaky <lutfilahdzaky12@gmail.com>
2022-04-25 23:32:57 +0700

    Add scope to AppModule

--- app/src/main/java/com/lutfilah/diplayground/AppModule.kt
+++ app/src/main/java/com/lutfilah/diplayground/AppModule.kt
@@ -6,7 +6,16 @@ import dagger.hilt.components.SingletonComponent
 
 /* Anotasi @Module
 * Berguna untuk memberitahukan Hilt class yang diberikan anotasi merupakan modul dependency untuk Hilt
+*
+* Anotasi Scope
+* Kita juga perlu menambahkan anotasi scope(cakupan) dari dependencynya dengan menggunakan anotasi @InstallIn(scope)
+* Contoh:
+* - SingletonComponent - Dependency di modul ini akan hidup selama aplikasi berjalan
+* - ActivityComponent - Maka modul/dependency di dalamnya akah hidup selama activity tertentu hidup
+* Lainnya bisa dilihat di dalam dokumentasi
+* (https://developer.android.com/training/dependency-injection/hilt-android)
 */
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 }
